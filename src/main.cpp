804fcb6766d26df2630e3039a25eeeb6ffd56036
iiisso <iiisoo@qq.com>
2023-05-16 14:24:34 -0400

    test.cpp done for simple test

--- src/main.cpp
+++ src/main.cpp
@@ -4,25 +4,52 @@
 
 int main() {
     // Create some students
-    Student s1("Alice", "Math", 20);
-    Student s2("Bob", "Computer Science", 20);
-    Student s3("Charlie", "Physics", 20);
+    Student s1("Davis", "MATH", 15);
+    Student s2("Walker", "COMPSCI", 15);
+    Student s3("Rivers", "COMPSCI", 15);
 
     // Create some classes
-    Class c1("Math101", 2, false); 
-    Class c2("CS101", 2, true); 
-    Class c3("Physics101", 2, true); 
+    Class c1("MATH131", 50, false); 
+    Class c2("COMPSCI377", 50, true); 
+    Class c3("ECON103", 1, true);
+    Class c4("COMPSCI121", 100, true);
 
     // Students add classes
-    std::cout << "Alice adds Math101: " << (s1.addClass(&c1) ? "Success" : "Failed") << std::endl;
-    std::cout << "Alice adds CS101: " << (s1.addClass(&c2) ? "Success" : "Failed") << std::endl;
-    std::cout << "Bob adds Math101: " << (s2.addClass(&c1) ? "Success" : "Failed") << std::endl;
-    std::cout << "Bob adds CS101: " << (s2.addClass(&c2) ? "Success" : "Failed") << std::endl;
-    std::cout << "Charlie adds Physics101: " << (s3.addClass(&c3) ? "Success" : "Failed") << std::endl;
+    s1.addClass(&c1);
+    s2.addClass(&c2);
+    s3.addClass(&c3);
+    s3.addClass(&c4);
 
     // Students drop classes
-    std::cout << "Alice drops Math101: " << (s1.dropClass(&c1) ? "Success" : "Failed") << std::endl;
-    std::cout << "Bob drops CS101: " << (s2.dropClass(&c2) ? "Success" : "Failed") << std::endl;
+    s2.dropClass(&c2);
+
+    // Students switch classes
+    s1.switchClass(&c1, &c4);
+
+    // Output classes' student names
+    std::cout << "Students in " << c1.getName() << ": ";
+    for(auto student : c1.getStudents()) {
+        std::cout << student->getName() << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Students in " << c2.getName() << ": ";
+    for(auto student : c2.getStudents()) {
+        std::cout << student->getName() << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Students in " << c3.getName() << ": ";
+    for(auto student : c3.getStudents()) {
+        std::cout << student->getName() << " ";
+    }
+    std::cout << std::endl;
+
+    std::cout << "Students in " << c4.getName() << ": ";
+    for(auto student : c4.getStudents()) {
+        std::cout << student->getName() << " ";
+    }
+    std::cout << std::endl;
 
     return 0;
 }
---
+++ src/test.cpp
@@ -0,0 +1,62 @@
+#include "class.h"
+#include "student.h"
+#include <iostream>
+
+void testAddClass() {
+    Student s("Test Student", "MATH", 15);
+    Class c("MATH101", 50, false);
+
+    if (!s.addClass(&c)) {
+        std::cout << "testAddClass failed: Expected to add class successfully." << std::endl;
+    }
+}
+
+void testDropClass() {
+    Student s("Test Student", "MATH", 15);
+    Class c("MATH101", 50, false);
+
+    s.addClass(&c);
+
+    if (!s.dropClass(&c)) {
+        std::cout << "testDropClass failed: Expected to drop class successfully." << std::endl;
+    }else {
+        std::cout << "testDropClass drops class successfully." << std::endl;
+    }
+}
+
+void testSwitchClass() {
+    Student s("Test Student", "MATH", 15);
+    Class c1("MATH101", 50, false);
+    Class c2("MATH102", 50, false);
+
+    s.addClass(&c1);
+
+    if (!s.switchClass(&c1, &c2)) {
+        std::cout << "testSwitchClass failed: Expected to switch class successfully." << std::endl;
+    } else {
+        std::cout << "testSwitchClass switchs class successfully." << std::endl;
+    }
+}
+
+void testClassCapacity() {
+    Student s1("Student1", "MATH", 15);
+    Student s2("Student2", "MATH", 15);
+    Class c("MATH101", 1, false);
+
+    s1.addClass(&c);
+
+    if (s2.addClass(&c)) {
+        std::cout << "testClassCapacity failed: Expected class to be full." << std::endl;
+    } else {
+        std::cout << "testClassCapacity success, class is full already." << std::endl;
+    }
+}
+
+int main() {
+    testAddClass();
+    testDropClass();
+    testSwitchClass();
+    testClassCapacity();
+
+    return 0;
+}
